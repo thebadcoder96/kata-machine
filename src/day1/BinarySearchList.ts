239ac43371faf9bc5e778a538db6bfe6ce92de77
Eminem <58310848+thebadcoder96@users.noreply.github.com>
2024-08-25 18:54:28 -0500

    init

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,17 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        const mid = Math.floor(low + (high - low) / 2);
+        if (haystack[mid] === needle) {
+            return true;
+        } else if (haystack[mid] < needle) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+
+    } while (low < high);
+    return false;
+}
