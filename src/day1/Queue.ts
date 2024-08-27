88d246f01c37e3595dbfbbac1d62b7ab2f4b290d
Eminem <58310848+thebadcoder96@users.noreply.github.com>
2024-08-26 23:59:10 -0500

    nice

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,45 @@
+type QNode<T> = {
+    value: T,
+    next?: QNode<T>,
+}
 export default class Queue<T> {
     public length: number;
+    private head?: QNode<T>;
+    private tail?: QNode<T>;
 
-    
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
-
+        const node = {value: item} as QNode<T>;
+        this.length++;
+        if (!this.tail ) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;   
 }
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        const node = this.head;
+        if (this.length === 0) {
+            this.head = this.tail = undefined;
+            return node.value;
+        }
+        this.head = this.head.next;
+
+        return node.value;
 
 }
     peek(): T | undefined {
+        return this.head?.value;
 
 }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
+type Node<T> = { 
+    value: T,
+    prev?: Node<T>,
+}
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
-
+        this.length++;
+        if (!this.head) {
+            this.head = { value: item } as Node<T>;
+        }
+        this.head = { value: item, prev: this.head } as Node<T>;
 }
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const value = this.head?.value;
+            this.head = undefined;
+            return value;
+        }
+        const value = this.head?.value;
+        this.head = this.head?.prev;
+        return value;
+
 
 }
     peek(): T | undefined {
-
+        return this.head?.value;
 }
 }
